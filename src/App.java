09fbc16649b67dd348da5e7409a73033b82fe417
kokic <3250679033@qq.com>
2022-07-15 18:28:29 +0800

    bin & hex

--- src/App.java
+++ src/App.java
@@ -2,14 +2,15 @@
 import static aira.Prelude.println;
 
 import pinkmonsoon.PinkMonsoon;
-import pinkmonsoon.TypePredicator;
+import static pinkmonsoon.TypePredicator.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        println.invoke(TypePredicator.isDecimalInt.invoke("1003917294"));
-        println.invoke(TypePredicator.isDecimalFrac.invoke("-3.1415926535"));
-
+        println.invoke(isDecimalInt.invoke("1003917294"));
+        println.invoke(isDecimalFrac.invoke("-3.1415926535"));
+        println.invoke(isHexInt.invoke("0x7a96"));
+        println.invoke(isBinInt.invoke("0b101"));
         println.invoke(PinkMonsoon.morphBool.map("."));
 
         println.invoke("Baby Pink Monsoon!");
--- src/pinkmonsoon/TypePredicator.java
+++ src/pinkmonsoon/TypePredicator.java
@@ -1,6 +1,6 @@
 package pinkmonsoon;
 
-import static aira.Prelude.foldl;
+import static aira.Prelude.foldl; 
 
 import aira.quasi.QuasiFunction.one_bool;
 import aira.quasi.QuasiFunction.one_t;
@@ -10,43 +10,46 @@ public final class TypePredicator {
 
     public interface With<T> extends two_t<Boolean, T, Boolean> {}
 
-    public static final two_t<With<Character>, String, Boolean> isDecimalPartial = 
-        (f, xs) -> (Boolean) foldl.invoke(f, true, PinkMonsoon.toCharList.invoke(xs));
-
     public static final two_t<String, Character, Boolean> isUniqueChar = 
         (xs, x) -> xs.indexOf(x) == xs.lastIndexOf(x);
-
-
+    
     public static final one_bool<Character> isPlusOrMinus = 
         x -> x == '+' || x == '-';
 
     public static final one_t<String, String> castUnsigned = 
         x -> isPlusOrMinus.invoke(x.charAt(0)) ? x.substring(1) : x;
+
+    public static final two_t<one_bool<Character>, String, Boolean> isUnsignedNumberPartial = 
+        (f, xs) -> (Boolean) foldl.invoke((With<Character>) (prev, x) -> prev && f.invoke(x), 
+            true, PinkMonsoon.toCharList.invoke(xs));
+
+    public static final two_t<one_bool<Character>, String, Boolean> isNumberPartial = 
+        (f, xs) -> isUnsignedNumberPartial.invoke(f, castUnsigned.invoke(xs));
     
-// int
 
     public static final one_bool<Character> isDecimalIntSingle = 
         x -> '0' <= x && x <= '9';
 
-    public static final With<Character> isDecimalIntWith = 
-        (prev, x) -> prev && isDecimalIntSingle.invoke(x);
-    
     public static final one_bool<String> isDecimalInt = 
-        xs -> isDecimalPartial.invoke(isDecimalIntWith, castUnsigned.invoke(xs));
+        xs -> isNumberPartial.invoke(isDecimalIntSingle, xs);
 
-// frac
-    public static final one_bool<Character> isDecimalFracSingle = 
-        x -> '.' == x || isDecimalIntSingle.invoke(x);
-    
-    public static final one_bool<String> isUniqueDot = 
-        x -> isUniqueChar.invoke(x, '.');
-
-    public static final With<Character> isDecimalFracWith = 
-        (prev, x) -> prev && isDecimalFracSingle.invoke(x);
 
     public static final one_bool<String> isDecimalFrac = 
-        xs -> isUniqueDot.invoke(xs) && isDecimalPartial.invoke(
-            isDecimalFracWith, castUnsigned.invoke(xs));
-        
+        xs -> isUniqueChar.invoke(xs, '.') && isNumberPartial.invoke(
+            x -> '.' == x || isDecimalIntSingle.invoke(x), 
+            castUnsigned.invoke(xs));
+            
+    public static final one_bool<String> isBinInt = 
+        s -> ((one_bool<String>) xs -> xs.charAt(0) == '0' && xs.charAt(1) == 'b' && 
+            isUnsignedNumberPartial.invoke(x -> x == '0' || x == '1', 
+            xs.substring(2))).invoke(castUnsigned.invoke(s).toLowerCase());
+
+    public static final one_bool<String> isHexInt = 
+        s -> ((one_bool<String>) xs -> xs.charAt(0) == '0' && xs.charAt(1) == 'x' && 
+            isUnsignedNumberPartial.invoke(x -> isDecimalIntSingle.invoke(x) || 
+                ('A' <= x && x <= 'F') || ('a' <= x && x <= 'f'), 
+            xs.substring(2))).invoke(castUnsigned.invoke(s).toLowerCase());
+    
+    
 }
 
